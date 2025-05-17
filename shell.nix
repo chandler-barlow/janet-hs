e9fe4e907494b8949cd94f8f27742fd50ed7bda2
Chandler Barlow <chandlerbrlw@gmail.com>
2025-05-17 15:36:42 -0700

    shell is working now

--- shell.nix
+++ shell.nix
@@ -1,16 +1,25 @@
 { pkgs ? import <nixpkgs> {} }:
 let 
   janet-headers = pkgs.callPackage ./janet-headers.nix {};
+  janet-hs = pkgs.haskellPackages.developPackage { 
+    name = "janet-hs";
+    root = ./.; 
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+        [ cabal-install
+          ghc
+        ]);
+    # returnShellEnv = true; 
+  };
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    cabal-install
-    ghc
     janet
     jpm
   ];
+  inputsFrom = [ janet-hs ];
   shellHook = ''
     echo "linking janet headers..."
-    ln -s ${janet-headers}/janet.h include/janet.h
+    ln -f -s ${janet-headers}/janet.h include/janet.h
   '';
 }
